997f4d4de2fd3e84ca01a4c4a494ba976b8850b2
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2014-02-10 12:04:40 +0100

    Add use of data files to example

--- source/fibcmd/main.cpp
+++ source/fibcmd/main.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include <template-version.h>
 #include <fiblib/Fibonacci.h>
@@ -8,6 +10,21 @@ int main(int /*argc*/, char* /*argv*/[])
 {
     std::cout << "Version: " << TEMPLATE_VERSION << std::endl;
     std::cout << "Fibonacci(8) = " << fiblib::Fibonacci()(8) << std::endl;
+	std::cout << std::endl;
 
-    return 0;
+    // Read file
+	std::cout << "Reading from 'data/example.txt': " << std::endl;
+	std::cout << std::endl;
+  	std::ifstream f("data/example.txt");
+  	if (f.is_open()) {
+  		std::string line;
+    	while (getline(f, line)) {
+      		std::cout << line << '\n';
+    	}
+    	f.close();
+  	} else {
+		std::cout << "Unable to open file." << std::endl;
+	}
+
+  	return 0;
 }
