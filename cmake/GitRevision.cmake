96b750c6ee421794253978178b8fdd910249b518
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2014-02-10 13:00:10 +0100

    Introduce INSTALL_ROOT to install files into the "root" installation directory

--- cmake/GitRevision.cmake
+++ cmake/GitRevision.cmake
@@ -1,19 +1,23 @@
+
 include(GetGitRevisionDescription)
 
 
 # Create file "revision" which contains the current git revision ID and
 # deploy this file during installation and packaging.
-
-macro(create_revision_file FILENAME)
+#
+# filename: Name of generated file
+# dest:     Destination path for installation
+# 
+macro(create_revision_file filename dest)
 
     # Add a revision file containing the git-head tag for cpack and install
     get_git_head_revision(GIT_REFSPEC GIT_SHA1)
 
     # Generate a shorter, googlelike variation for rev
     string(SUBSTRING ${GIT_SHA1} 0 12 GIT_REV)
-    file(WRITE ${FILENAME} ${GIT_REV})
+    file(WRITE ${filename} ${GIT_REV})
 
     # Install
-    install(FILES ${FILENAME} DESTINATION ".")
+    install(FILES ${filename} DESTINATION ${dest})
 
 endmacro()
