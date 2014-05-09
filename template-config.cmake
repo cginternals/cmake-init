96e912219bc902c30146c08e3dd85583fd8ecb38
Sebastian Kay Belle <sebastian.belle@hpi.uni-potsdam.de>
2014-05-09 10:47:51 +0200

    Minimal required CMake version now 2.8.12. CMake init scripts now uses functions instead of macros. Finally, some clean-up and streamlining.

--- template-config.cmake
+++ template-config.cmake
@@ -1,44 +1,29 @@
 
-# TEMPLATE_FOUND
+######
+# This is the find-script for your project/library used by CMake if this project/libary is to be
+# included in another project/library.
+######
 
-# TEMPLATE_LIBRARIES
-# TEMPLATE_INCLUDES
+#
+# Renaming TEMPLATE... -> <PROJECT_NAME>...
+#
+# TEMPLATE_DIR -> <PROJECT_NAME>_DIR
+# TEMPLATE_FOUND -> <PROJECT_NAME>_FOUND
+# TEMPLATE_LIBRARIES -> <PROJECT_NAME>_LIBRARIES
+# TEMPLATE_INCLUDES -> <PROJECT_NAME>_INCLUDES
 
-# TEMPLATE_FIBLIB_LIBRARY
-# TEMPLATE_FIBLIB_LIBRARY_RELEASE
-# TEMPLATE_FIBLIB_LIBRARY_DEBUG
-# TEMPLATE_FIBLIB_INCLUDE_DIR
+
+# TEMPLATE_FIBLIB_LIBRARY -> <PROJECT_NAME>_<...>_LIBRARY
+# TEMPLATE_FIBLIB_LIBRARY_RELEASE -> <PROJECT_NAME>_<...>_LIBRARY_RELEASE
+# TEMPLATE_FIBLIB_LIBRARY_DEBUG -> <PROJECT_NAME>_<...>_LIBRARY_DEBUG
+# TEMPLATE_FIBLIB_INCLUDE_DIR -> <PROJECT_NAME>_<...>_INCLUDE_DIR
 
 
-include(FindPackageHandleStandardArgs)
 
-if(CMAKE_CURRENT_LIST_FILE)
-    get_filename_component(TEMPLATE_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-endif()
 
-file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" ENVPROGRAMFILES)
-file(TO_CMAKE_PATH "$ENV{TEMPLATE_DIR}" ENVTEMPLATE_DIR)
+# Definition of function "find" with two mandatory arguments, "LIB_NAME" and "HEADER".
+function (find LIB_NAME HEADER)
 
-set(LIB_PATHS   
-    ${TEMPLATE_DIR}/build
-    ${TEMPLATE_DIR}/build/Release
-    ${TEMPLATE_DIR}/build/Debug
-    ${TEMPLATE_DIR}/build-release
-    ${TEMPLATE_DIR}/build-debug
-    ${ENVTEMPLATE_DIR}/lib
-    ${TEMPLATE_DIR}/lib
-    ${ENVPROGRAMFILES}/template/lib
-    /usr/lib
-    /usr/local/lib
-    /sw/lib
-    /opt/local/lib
-    /usr/lib64
-    /usr/local/lib64
-    /sw/lib64
-    /opt/local/lib64
-)
-
-macro (find LIB_NAME HEADER)
     set(HINT_PATHS ${ARGN})
 
     if (${LIB_NAME} STREQUAL "template")
@@ -49,27 +34,36 @@ macro (find LIB_NAME HEADER)
         set(LIBNAME ${LIB_NAME})
     endif()
 
-    find_path(${LIB_NAME_UPPER}_INCLUDE_DIR ${HEADER}
-        ${ENVTEMPLATE_DIR}/include
-        ${ENVTEMPLATE_DIR}/source/${LIB_NAME}/include
+    find_path(
+	${LIB_NAME_UPPER}_INCLUDE_DIR
+	${HEADER}
+        ${ENV_TEMPLATE_DIR}/include
+        ${ENV_TEMPLATE_DIR}/source/${LIB_NAME}/include
         ${TEMPLATE_DIR}/include
         ${TEMPLATE_DIR}/source/${LIB_NAME}/include
-        ${ENVPROGRAMFILES}/template/include
+        ${ENV_PROGRAMFILES}/digvis/include
         /usr/include
         /usr/local/include
         /sw/include
         /opt/local/include
-        DOC "The directory where ${HEADER} resides")
+        DOC "The directory where ${HEADER} resides"
+    )
 
-    find_library(${LIB_NAME_UPPER}_LIBRARY_RELEASE
+
+    find_library(
+	${LIB_NAME_UPPER}_LIBRARY_RELEASE
         NAMES ${LIBNAME}
         PATHS ${HINT_PATHS}
-        DOC "The ${LIB_NAME} library")
-    find_library(${LIB_NAME_UPPER}_LIBRARY_DEBUG
+        DOC "The ${LIB_NAME} library"
+    )
+    find_library(
+	${LIB_NAME_UPPER}_LIBRARY_DEBUG
         NAMES ${LIBNAME}d
         PATHS ${HINT_PATHS}
-        DOC "The ${LIB_NAME} debug library")
+        DOC "The ${LIB_NAME} debug library"
+    )
     
+
     if(${LIB_NAME_UPPER}_LIBRARY_RELEASE AND ${LIB_NAME_UPPER}_LIBRARY_DEBUG)
         set(${LIB_NAME_UPPER}_LIBRARY "optimized" ${${LIB_NAME_UPPER}_LIBRARY_RELEASE} "debug" ${${LIB_NAME_UPPER}_LIBRARY_DEBUG})
     elseif(${LIB_NAME_UPPER}_LIBRARY_RELEASE)
@@ -78,16 +72,53 @@ macro (find LIB_NAME HEADER)
         set(${LIB_NAME_UPPER}_LIBRARY ${${LIB_NAME_UPPER}_LIBRARY_DEBUG})
     endif()
 
-    set(TEMPLATE_INCLUDES  ${TEMPLATE_INCLUDES}  ${${LIB_NAME_UPPER}_INCLUDE_DIR})
-    set(TEMPLATE_LIBRARIES ${TEMPLATE_LIBRARIES} ${${LIB_NAME_UPPER}_LIBRARY})
 
-    # DEBUG
-    #message("${LIB_NAME_UPPER}_INCLUDE_DIR     = ${${LIB_NAME_UPPER}_INCLUDE_DIR}")
-    #message("${LIB_NAME_UPPER}_LIBRARY_RELEASE = ${${LIB_NAME_UPPER}_LIBRARY_RELEASE}")
-    #message("${LIB_NAME_UPPER}_LIBRARY_DEBUG   = ${${LIB_NAME_UPPER}_LIBRARY_DEBUG}")
-    #message("${LIB_NAME_UPPER}_LIBRARY         = ${${LIB_NAME_UPPER}_LIBRARY}")
+    set(TEMPLATE_INCLUDES  ${TEMPLATE_INCLUDES}  ${${LIB_NAME_UPPER}_INCLUDE_DIR} PARENT_SCOPE)
+    set(TEMPLATE_LIBRARIES ${TEMPLATE_LIBRARIES} ${${LIB_NAME_UPPER}_LIBRARY} PARENT_SCOPE)
 
-endmacro()
+    # DEBUG MESSAGES
+#    message("${LIB_NAME_UPPER}_INCLUDE_DIR     = ${${LIB_NAME_UPPER}_INCLUDE_DIR}")
+#    message("${LIB_NAME_UPPER}_LIBRARY_RELEASE = ${${LIB_NAME_UPPER}_LIBRARY_RELEASE}")
+#    message("${LIB_NAME_UPPER}_LIBRARY_DEBUG   = ${${LIB_NAME_UPPER}_LIBRARY_DEBUG}")
+#    message("${LIB_NAME_UPPER}_LIBRARY         = ${${LIB_NAME_UPPER}_LIBRARY}")
+
+endfunction(find)
+
+
+
+
+
+
+
+
+# load standard CMake arguments (c.f. http://stackoverflow.com/questions/7005782/cmake-include-findpackagehandlestandardargs-cmake)
+include(FindPackageHandleStandardArgs)
+
+if(CMAKE_CURRENT_LIST_FILE)
+    get_filename_component(TEMPLATE_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+endif()
+
+file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" ENV_PROGRAMFILES)
+file(TO_CMAKE_PATH "$ENV{TEMPLATE_DIR}" ENV_TEMPLATE_DIR)
+
+set(LIB_PATHS   
+    ${TEMPLATE_DIR}/build
+    ${TEMPLATE_DIR}/build/Release
+    ${TEMPLATE_DIR}/build/Debug
+    ${TEMPLATE_DIR}/build-release
+    ${TEMPLATE_DIR}/build-debug
+    ${TEMPLATE_DIR}/lib
+    ${ENV_TEMPLATE_DIR}/lib
+    ${ENV_PROGRAMFILES}/template/lib
+    /usr/lib
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    /usr/lib64
+    /usr/local/lib64
+    /sw/lib64
+    /opt/local/lib64
+)
 
 
 # Find libraries
