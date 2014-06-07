4404e7d9c8d57ea652178593a661b140dd278a79
Daniel Limberger <daniel.limberger@hpi.uni-potsdam.de>
2014-06-07 09:05:13 +0200

    remove api warning for stl exports

--- source/fiblib/include/fiblib/fiblib_api.h
+++ source/fiblib/include/fiblib/fiblib_api.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// NOTE: don't export stl stuff (e.g. containers):
-// http://www.unknownroad.com/rtfm/VisualStudio/warningC4251.html
-// don't do it: http://support.microsoft.com/kb/q168958/
-
 #ifdef _MSC_VER
 #   define FIBLIB_API_EXPORT_DECLARATION __declspec(dllexport)
 #   define FIBLIB_API_IMPORT_DECLARATION __declspec(dllimport)
