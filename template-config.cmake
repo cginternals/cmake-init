bd8857be43f369db5012d307f2ec3ee487d9f121
Stefan Buschmann <stefan.buschmann@hpi.de>
2015-11-21 21:24:52 +0100

    Create find script version that can be used for devtree and installed location

--- template-config.cmake
+++ template-config.cmake
@@ -1,20 +1,41 @@
 
-# This config script tries to locate a project in its source tree.
-# For installed version, see deploy/template-config.cmake
+# This config script tries to locate the project either in its source tree
+# of from an install location.
+# 
+# Please adjust the list of submodules to search for.
 
 
-# Macro for loading a target configuration if it exists
-macro(include_config_script PREFIX FILENAME)
-    set(filename "${CMAKE_CURRENT_LIST_DIR}/${PREFIX}/${FILENAME}")
-    if(EXISTS "${filename}")
-        include("${filename}")
+# List of modules
+set(MODULE_NAMES
+    baselib
+    fiblib
+)
+
+
+# Macro to search for a specific module
+macro(find_module FILENAME)
+    if(EXISTS "${FILENAME}")
+        set(MODULE_FOUND TRUE)
+        include("${FILENAME}")
     endif()
 endmacro()
 
+# Macro to search for all modules
+macro(find_modules PREFIX)
+    foreach(module_name ${MODULE_NAMES})
+        find_module("${CMAKE_CURRENT_LIST_DIR}/${PREFIX}/${module_name}/${module_name}-export.cmake")
+    endforeach(module_name)
+endmacro()
+
+
+# Try install location
+set(MODULE_FOUND FALSE)
+find_modules("cmake")
+
+if(MODULE_FOUND)
+    return()
+endif()
 
 # Try common build locations
-set(prefix "build/source")
-include("${CMAKE_CURRENT_LIST_DIR}/template-files.cmake")
-
-set(prefix "build-debug/source")
-include("${CMAKE_CURRENT_LIST_DIR}/template-files.cmake")
+find_modules("build/source")
+find_modules("build-debug/source")
--- template-files.cmake
+++
@@ -1,4 +0,0 @@
-
-# Find project targets
-include_config_script("${prefix}" "baselib/baselib-export.cmake")
-include_config_script("${prefix}" "fiblib/fiblib-export.cmake")
