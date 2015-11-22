57df819686c11b02bafc82ac5b669baa19f157e2
Stefan Buschmann <stefan.buschmann@hpi.de>
2015-11-22 14:49:35 +0100

    Add global configuration script for platform specific compiler options etc.

---
+++ cmake/CompileOptions.cmake
@@ -0,0 +1,54 @@
+
+# 
+# Identify platform
+# 
+
+if(WIN32 OR MINGW)
+    set(PLATFORM_WINDOWS 1)
+elseif(APPLE)
+    set(PLATFORM_APPLE 1)
+elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    set(PLATFORM_LINUX 1)
+endif()
+if(UNIX)
+    set(PLATFORM_UNIX 1)
+endif()
+
+
+# 
+# Platform and architecture setup
+# 
+
+# Architecture (32/64 bit)
+set(X64 OFF)
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(X64 ON)
+endif()
+
+
+# 
+# Compile definitions
+# 
+
+set(DEFAULT_COMPILE_DEFINITIONS
+)
+
+
+# 
+# Compile options
+# 
+
+set(DEFAULT_COMPILE_OPTIONS
+)
+
+
+# 
+# System libraries
+# 
+
+# Use pthreads on linux and mingw
+if(PLATFORM_LINUX OR MINGW)
+    set(SYSTEM_LIBRARIES
+        pthread
+    )
+endif()
