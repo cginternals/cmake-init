32f2b519e23e9bb165d981a5f7b5d73dff8c869d
Willy Scheibel <willy.scheibel@hpi.de>
2015-12-02 18:17:22 +0100

    Remove simple to fix warnings

--- source/examples/fibgui/MainWindow.cpp
+++ source/examples/fibgui/MainWindow.cpp
@@ -15,29 +15,29 @@ MainWindow::MainWindow()
     setCentralWidget(content);
 
     // Create layout
-    QBoxLayout * layout = new QVBoxLayout();
-    content->setLayout(layout);
+    QBoxLayout * boxLayout = new QVBoxLayout();
+    content->setLayout(boxLayout);
 
     // Add title
     QLabel * title = new QLabel(content);
     title->setText("Please enter n:");
-    layout->addWidget(title);
+    boxLayout->addWidget(title);
 
     // Add input field
     QSpinBox * editNumber = new QSpinBox(content);
     editNumber->setMinimum(0);
-    layout->addWidget(editNumber);
+    boxLayout->addWidget(editNumber);
 
     // Add result
     QLabel * result = new QLabel(content);
     result->setText("Fib(0) = 0");
-    layout->addWidget(result);
+    boxLayout->addWidget(result);
 
     // When input changes, calculate and output the fibonacci number
     connect(editNumber, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), [result] (int n)
     {
         fiblib::Fibonacci fib;
-        result->setText("Fib(" + QString::number(n) + ") = " + QString::number(fib(n)));
+        result->setText("Fib(" + QString::number(n) + ") = " + QString::number(fib(static_cast<unsigned int>(n))));
     });
 }
 
