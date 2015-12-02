48635a9c2699e9dc8aa1f8b6a1a409e0d7be5247
Willy Scheibel <willy.scheibel@hpi.de>
2015-12-02 22:55:59 +0100

    First proposal for MSVC compiler options

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -60,6 +60,53 @@ set(DEFAULT_COMPILE_DEFINITIONS)
 
 set(DEFAULT_COMPILE_OPTIONS)
 
+if (PLATFORM_WINDOWS AND MSVC)
+    set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
+        /MP # -> build with multiple processes
+        /nologo       # -> no compiler banner
+        /Zc:wchar_t   # -> treat wchar_t as built-in type: yes
+        /Zc:forScope  # -> force conformance in for loop scope: Yes
+        /Gm           # -> enable minimal rebuild
+        /fp:precise   # -> floating point model: precise
+        /arch:SSE2    # -> enable enhanced instruction set: streaming simd extensions 2
+
+        # /W4           # -> warning level 4
+        /Wall         # -> enable all warnings
+        
+      # Disable the following warnings
+      # /wd4273       # -> Two definitions in a file differ in their use of dllimport.
+      # /wd4100       # -> 'identifier' : unreferenced formal parameter
+      # /wd4127       # -> conditional expression is constant
+      # /wd4251       # -> 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
+      # /wd4267       # -> 'var' : conversion from 'size_t' to 'type', possible loss of data
+        
+        /vmg          # ->  to declare a pointer to a member of a class before defining the class. This need can arise if you define members in two different classes that reference each other.
+        
+        $<$<CONFIG:Debug>:   
+            /RTC1         # -> Runtime error checks
+            /RTCc
+            /Od           # -> Optimization: none
+            /GS           # -> buffer security check
+            /GF-          # -> disable string pooling
+            /Zi           # -> debug information format: program database
+        >
+        $<$<CONFIG:Release>: 
+            /Ox           # -> optimization: full optimization
+            /Ob2          # -> inline function expansion: any suitable
+            /Oi           # -> enable intrinsic functions: yes
+            /Ot           # -> favor size or speed: favor fast code
+            /Oy           # -> omit frame pointers: yes
+          # /Og           # -> use global optimization
+          # /Gw           # -> whole program global optimization
+          
+            /GS-          # -> buffer security check: no 
+            /GL           # -> whole program optimization: enable link-time code generation
+            /GF           # -> enable string pooling
+            /GR           # -> runtime type information          
+        >
+    )
+endif ()
+
 if (PLATFORM_LINUX AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU") # GCC 4.9 and 5.1
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
         -Wall
