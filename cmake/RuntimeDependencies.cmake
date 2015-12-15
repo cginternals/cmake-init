1296670769cd52473a351aad2c626c8ea8cc650f
Stefan Buschmann <stefan.buschmann@hpi.de>
2015-12-15 16:58:34 +0100

    Add example for using GetPrerequisites

--- cmake/RuntimeDependencies.cmake
+++ cmake/RuntimeDependencies.cmake
@@ -1,7 +1,4 @@
 
-include(GetPrerequisites)
-
-
 # 
 # Default dependencies for the runtime-package
 # 
@@ -20,3 +17,26 @@ if(OPTION_SELF_CONTAINED)
     # install(FILES ... COMPONENT runtime)
 
 endif()
+
+
+
+# Paths to executables
+set(apps
+    "\${CMAKE_INSTALL_PREFIX}/${INSTALL_BIN}/fibgui${CMAKE_EXECUTABLE_SUFFIX}"
+)
+
+# run Bundle utilities
+install(CODE "
+    include(\"${CMAKE_ROOT}/Modules/GetPrerequisites.cmake\")
+
+    foreach(app ${apps})
+        message(\"Dependencies for \${app}\")
+
+        get_prerequisites(\${app} prereqs 1 1 \"\" \"\")
+        foreach(lib \${prereqs})
+            message(\"- \${lib}\")
+        endforeach(lib)
+    endforeach(app)
+    "
+    COMPONENT runtime
+)
