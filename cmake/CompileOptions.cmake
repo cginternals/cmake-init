85ebbfcf06c81a3c4375909b0f48255702b95d1a
Willy Scheibel <willy.scheibel@cginternals.com>
2016-03-13 22:17:44 +0100

    Fix generated feature headers and add c++11 flag explicitly if cmake version is too low

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -123,6 +123,10 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCH
         $<$<PLATFORM_ID:Darwin>:
             -pthread
         >
+        
+        $<$<VERSION_LESS:${CMAKE_VERSION},3.2>:
+            -std=c++11
+        >
     )
 endif ()
 
