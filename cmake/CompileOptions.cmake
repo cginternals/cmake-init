56e177454d0efb998aab51c13b0dcfbf97b4d9ec
Willy Scheibel <willy.scheibel@hpi.de>
2019-04-26 19:25:33 +0200

    Review default visibility of compiler and linker options

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -67,75 +67,79 @@ set(DEFAULT_COMPILE_OPTIONS)
 # MSVC compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
-        /MP           # -> build with multiple processes
-        /W4           # -> warning level 4
-        # /WX         # -> treat warnings as errors
+        PRIVATE
+            /MP           # -> build with multiple processes
+            /W4           # -> warning level 4
+            # /WX         # -> treat warnings as errors
 
-        /wd4251       # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
-        /wd4592       # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
-        # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
-        # /wd4127     # -> disable warning: conditional expression is constant (caused by Qt)
-        
-        #$<$<CONFIG:Debug>:
-        #/RTCc         # -> value is assigned to a smaller data type and results in a data loss
-        #>
+            #$<$<CONFIG:Debug>:
+            #/RTCc         # -> value is assigned to a smaller data type and results in a data loss
+            #>
 
-        $<$<CONFIG:Release>: 
-        /Gw           # -> whole program global optimization
-        /GS-          # -> buffer security check: no 
-        /GL           # -> whole program optimization: enable link-time code generation (disables Zi)
-        /GF           # -> enable string pooling
-        >
-        
-        # No manual c++11 enable for MSVC as all supported MSVC versions for cmake-init have C++11 implicitly enabled (MSVC >=2013)
+            $<$<CONFIG:Release>: 
+            /Gw           # -> whole program global optimization
+            /GS-          # -> buffer security check: no 
+            /GL           # -> whole program optimization: enable link-time code generation (disables Zi)
+            /GF           # -> enable string pooling
+            >
+            
+            # No manual c++11 enable for MSVC as all supported MSVC versions for cmake-init have C++11 implicitly enabled (MSVC >=2013)
+
+        PUBLIC
+            /wd4251       # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
+            /wd4592       # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
+            # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
+            # /wd4127     # -> disable warning: conditional expression is constant (caused by Qt)
     )
 endif ()
 
 # GCC and Clang compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
-        -Wall
-        -Wextra
-        -Wunused
+        PRIVATE
+            -Wall
+            -Wextra
+            -Wunused
 
-        -Wreorder
-        -Wignored-qualifiers
-        -Wmissing-braces
-        -Wreturn-type
-        -Wswitch
-        -Wswitch-default
-        -Wuninitialized
-        -Wmissing-field-initializers
-        
-        $<$<CXX_COMPILER_ID:GNU>:
-            -Wmaybe-uninitialized
+            -Wreorder
+            -Wignored-qualifiers
+            -Wmissing-braces
+            -Wreturn-type
+            -Wswitch
+            -Wswitch-default
+            -Wuninitialized
+            -Wmissing-field-initializers
             
-            $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,4.8>:
+            $<$<CXX_COMPILER_ID:GNU>:
+                -Wmaybe-uninitialized
+                
+                $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,4.8>:
+                    -Wpedantic
+                    
+                    -Wreturn-local-addr
+                >
+            >
+            
+            $<$<CXX_COMPILER_ID:Clang>:
                 -Wpedantic
                 
-                -Wreturn-local-addr
+                # -Wreturn-stack-address # gives false positives
             >
-        >
-        
-        $<$<CXX_COMPILER_ID:Clang>:
-            -Wpedantic
             
-            # -Wreturn-stack-address # gives false positives
-        >
-        
-        $<$<PLATFORM_ID:Darwin>:
-            -pthread
-        >
-        
-        # Required for CMake < 3.1; should be removed if minimum required CMake version is raised.
-        $<$<VERSION_LESS:${CMAKE_VERSION},3.1>:
-            -std=c++11
-        >
-        
-        $<$<BOOL:${OPTION_COVERAGE_ENABLED}>:
-            -fprofile-arcs
-            -ftest-coverage
-        >
+            $<$<BOOL:${OPTION_COVERAGE_ENABLED}>:
+                -fprofile-arcs
+                -ftest-coverage
+            >
+            
+        PUBLIC
+            $<$<PLATFORM_ID:Darwin>:
+                -pthread
+            >
+            
+            # Required for CMake < 3.1; should be removed if minimum required CMake version is raised.
+            $<$<VERSION_LESS:${CMAKE_VERSION},3.1>:
+                -std=c++11
+            >
     )
 endif ()
 
@@ -149,15 +153,17 @@ set(DEFAULT_LINKER_OPTIONS)
 # Use pthreads on mingw and linux
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
     set(DEFAULT_LINKER_OPTIONS
-        ${DEFAULT_LINKER_OPTIONS}
-        -pthread
+        PUBLIC
+            ${DEFAULT_LINKER_OPTIONS}
+            -pthread
     )
     
     if (${OPTION_COVERAGE_ENABLED})
         set(DEFAULT_LINKER_OPTIONS
-        ${DEFAULT_LINKER_OPTIONS}
-        -fprofile-arcs
-        -ftest-coverage
-    )
+            PUBLIC
+                ${DEFAULT_LINKER_OPTIONS}
+                -fprofile-arcs
+                -ftest-coverage
+        )
     endif ()
 endif()
