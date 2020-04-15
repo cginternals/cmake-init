55bf7e82380cec0b489c04675fc05942c0afea0a
Willy Scheibel <willy.scheibel@hpi.de>
2020-04-15 19:30:47 +0200

    Cleanup

--- cmake/Findgoogletest.cmake
+++ cmake/Findgoogletest.cmake
@@ -1,21 +1,19 @@
 
 # googletest_FOUND
-# Target googletest-dev
-
-# GLEW_BINARY (win32 only)
+# Target googletest::googletest
 
 include(FindPackageHandleStandardArgs)
 
-find_path(GOOGLETEST_PROJECT_DIR googletest/googletest/include/gtest/gtest.h
+find_path(GOOGLETEST_PROJECT_DIR googletest/include/gtest/gtest.h
     PATHS
     $ENV{GOOGLETEST_DIR}
     /usr/src
     /usr/local/src
 
     PATH_SUFFIXES
-    /include
+    /googletest
 
-    DOC "The directory where googletest/googletest/include/gtest/gtest.h resides")
+    DOC "The directory where googletest/include/gtest/gtest.h resides")
 
 find_package_handle_standard_args(googletest REQUIRED_VARS GOOGLETEST_PROJECT_DIR)
 mark_as_advanced(GOOGLETEST_PROJECT_DIR)
@@ -29,18 +27,18 @@ if (googletest_FOUND)
     set(gtest_force_shared_crt      ON  CACHE BOOL "")
     set(gtest_hide_internal_symbols OFF CACHE BOOL "")
 
-    add_subdirectory("${GOOGLETEST_PROJECT_DIR}/googletest" "${PROJECT_BINARY_DIR}/googletest")
+    add_subdirectory("${GOOGLETEST_PROJECT_DIR}" "${PROJECT_BINARY_DIR}/googletest")
     
     # Create interface library to link against gmock
-    add_library(googletest INTERFACE)
+    add_library(googletest::googletest INTERFACE IMPORTED)
 
-    target_include_directories(googletest
+    target_include_directories(googletest::googletest
         SYSTEM INTERFACE
-        ${GOOGLETEST_PROJECT_DIR}/googletest/googletest/include
-        ${GOOGLETEST_PROJECT_DIR}/googletest/googlemock/include
+        ${GOOGLETEST_PROJECT_DIR}/googletest/include
+        ${GOOGLETEST_PROJECT_DIR}/googlemock/include
     )
 
-    target_link_libraries(googletest
+    target_link_libraries(googletest::googletest
         INTERFACE
         gmock
     )
