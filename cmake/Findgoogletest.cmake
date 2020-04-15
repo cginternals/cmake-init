0571c02c1f1210a7f8e31fde6750e8fbda3b8c6a
Willy Scheibel <willy.scheibel@hpi.de>
2020-04-15 19:09:02 +0200

    Replace in-source googletest with external googletest setup

---
+++ cmake/Findgoogletest.cmake
@@ -0,0 +1,47 @@
+
+# googletest_FOUND
+# Target googletest-dev
+
+# GLEW_BINARY (win32 only)
+
+include(FindPackageHandleStandardArgs)
+
+find_path(GOOGLETEST_PROJECT_DIR googletest/googletest/include/gtest/gtest.h
+    PATHS
+    $ENV{GOOGLETEST_DIR}
+    /usr/src
+    /usr/local/src
+
+    PATH_SUFFIXES
+    /include
+
+    DOC "The directory where googletest/googletest/include/gtest/gtest.h resides")
+
+find_package_handle_standard_args(googletest REQUIRED_VARS GOOGLETEST_PROJECT_DIR)
+mark_as_advanced(GOOGLETEST_PROJECT_DIR)
+
+if (googletest_FOUND)
+    # Build gmock
+    set(gmock_build_tests           OFF CACHE BOOL "")
+    set(gtest_build_samples         OFF CACHE BOOL "")
+    set(gtest_build_tests           OFF CACHE BOOL "")
+    set(gtest_disable_pthreads      OFF CACHE BOOL "")
+    set(gtest_force_shared_crt      ON  CACHE BOOL "")
+    set(gtest_hide_internal_symbols OFF CACHE BOOL "")
+
+    add_subdirectory("${GOOGLETEST_PROJECT_DIR}/googletest" "${PROJECT_BINARY_DIR}/googletest")
+    
+    # Create interface library to link against gmock
+    add_library(googletest INTERFACE)
+
+    target_include_directories(googletest
+        SYSTEM INTERFACE
+        ${GOOGLETEST_PROJECT_DIR}/googletest/googletest/include
+        ${GOOGLETEST_PROJECT_DIR}/googletest/googlemock/include
+    )
+
+    target_link_libraries(googletest
+        INTERFACE
+        gmock
+    )
+endif ()
